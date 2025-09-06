f62e62891222788831a070505458c9009d3264d3
timurGayberi <mehmet.gayberi@edu.futuregames.se>
2025-09-06 18:45:24 +0200

    getting started

---
+++ Assets/Scripts/generalScripts/Test.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace generalScripts
+{
+    public class Test : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Test");
+        }
+    }
+
+}
